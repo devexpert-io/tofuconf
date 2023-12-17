6cd6fe329380e0e553fe0607f1e914333894f92e
antoniolg <antonio@devexperto.com>
2023-12-17 18:29:31 +0100

    Update schedule

--- src/scripts/schedule.ts
+++ src/scripts/schedule.ts
@@ -5,18 +5,18 @@ export interface ScheduleItem {
 }
 
 const schedule: ScheduleItem[] = [
-    { time: '9:30', title: 'Registro y café', isSpecial: true },
-    { time: '10:00', title: 'TBD Talk' },
-    { time: '10:00', title: 'Charla TBD' },
-    { time: '11:00', title: 'Charla TBD' },
-    { time: '12:00', title: 'Charla TBD' },
+    { time: '10:00', title: 'Registro y café', isSpecial: true },
+    { time: '10:30', title: 'Apertura', isSpecial: true },
+    { time: '10:45', title: 'Charla TBD' },
+    { time: '11:30', title: 'Charla TBD' },
+    { time: '12:15', title: 'Charla TBD' },
     { time: '13:00', title: 'Charla TBD' },
     { time: '14:00', title: 'Comida', isSpecial: true },
     { time: '15:30', title: 'Charla TBD' },
-    { time: '16:30', title: 'Charla TBD' },
-    { time: '17:30', title: 'Charla TBD' },
-    { time: '18:30', title: 'Charla TBD' },
-    { time: '19:30', title: 'Cierre', isSpecial: true },
+    { time: '16:15', title: 'Charla TBD' },
+    { time: '17:00', title: 'Charla TBD' },
+    { time: '17:45', title: 'Charla TBD' },
+    { time: '18:30', title: 'Cierre', isSpecial: true },
 ];
 
 export default schedule;
